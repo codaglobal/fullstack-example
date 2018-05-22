ab575bcf24c36928d346e4d02cb22f479d098aaa
chaitanya11 <chaitanya.garikipati38@gmail.com>
2018-05-22 14:49:20 +0530

    lanching only head less chomre for testing
    lanching only head less chomre for testing

--- webui/karma.conf.js
+++ webui/karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
     // you can define custom flags
     customLaunchers: {
